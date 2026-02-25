8be7348af3c97c98714a4c95e2ed30ce11daf0c7
Favour <favourolaosebikanf>
2026-02-25 21:41:02 +0100

    favour

--- mastery-ai/src/App.jsx
+++ mastery-ai/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Quizes from "./component/quizes";
+import InProgress from "./component/in-progress";
+import CompletedPage from "./component/completed";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+  <Route path="/" element={<Navigate to="/quiz/1" replace />} />
+
+  <Route path="/quiz/:quizId" element={<Quizes />} />
+  <Route path="/quiz/:quizId/in-progress" element={<InProgress />} />
+  <Route path="/quiz/:quizId/completed" element={<CompletedPage />} />
+</Routes>
+  );
 }
 
-export default App
+export default App;
+
+// import { Routes, Route } from "react-router-dom";
+// import Quizes from "./pages/Quizes";
+// import InProgress from "./pages/InProgress";
+// import CompletedPage from "./pages/CompletedPage";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/quiz/:quizId" element={<Quizes />} />
+//       <Route path="/quiz/:quizId/in-progress" element={<InProgress />} />
+//       <Route path="/quiz/:quizId/completed" element={<CompletedPage />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
