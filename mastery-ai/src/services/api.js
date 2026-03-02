9fa6745667f1172e91f089d7a6c1ca506156e69f
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2026-03-02 10:59:13 +0100

    New

--- mastery-ai/src/services/api.js
+++ mastery-ai/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL; // Imports the url set in the environment
 
 export const fetchUserProfile = async (token) => {
   const controller = new AbortController();
